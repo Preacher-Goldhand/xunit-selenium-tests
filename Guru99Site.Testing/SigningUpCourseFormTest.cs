becbe7591935a3e14dc1e5a1f7f590537295d6c4
Mateusz Hanulok <85747046+Preacher-Goldhand@users.noreply.github.com>
2022-12-09 12:00:51 +0100

    refactoring

--- Guru99Site.Testing/SigningUpCourseFormTest.cs
+++ Guru99Site.Testing/SigningUpCourseFormTest.cs
@@ -2,78 +2,51 @@ using FluentAssertions;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using SeleniumExtras.PageObjects;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace Guru99Site.Testing
 {
     public class SigningUpCourseFormTest
     {
-        private static WebDriver _chromeDriver;
+        private static IWebDriver _chromeDriver;
         private static readonly string _url = "http://demo.guru99.com/test/guru99home/";
-
-        // Find WebElements on the page
-        [FindsBy(How = How.XPath, Using = ".//*[@id='philadelphia-field-email']")]
-        [CacheLookup]
-        private WebElement _emailText;
-
-        [FindsBy(How = How.Id, Using = "awf_field-91977689")]
-        [CacheLookup]
-        private WebElement _course;
-
-        [FindsBy(How = How.XPath, Using = ".//*[@id='philadelphia-field-submit']")]
-        [CacheLookup]
-        private WebElement _submitBtn;
+        private static int WAIT = 10;
 
         [Fact]
         public void SigningUpForCourseSuccessfully()
         {
-            FillInForm();
-            AssertResult();
-            Quit();
-        }
+            // Set up the ChromeDriver instance
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver _chromeDriver = new ChromeDriver();
+            _chromeDriver.Url = _url;
+            _chromeDriver.Manage().Window.Maximize();
+            _chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(WAIT);
 
-        // Set up the ChromeDriver instance
-        [Fact]
-        public static WebDriver GetInstance()
-        {
-            if (_chromeDriver == null)
-            {
-                _chromeDriver = new ChromeDriver("C:\\Users\\rench\\Desktop\\Programming\\Testing_Course\\ChromeDriver");
-                _chromeDriver.Url = _url;
-                _chromeDriver.Manage().Window.Maximize();
-            }
-            return _chromeDriver;
-        }
+            // Arrange
+            WebElement emailText = (WebElement)_chromeDriver.FindElement(By.XPath(".//*[@id='philadelphia-field-email']"));
+            WebElement course = (WebElement)_chromeDriver.FindElement(By.Id("awf_field-91977689"));
+            WebElement submitBtn = (WebElement)_chromeDriver.FindElement(By.XPath(".//*[@id='philadelphia-field-submit']"));
 
-        // Cleaning up
-        [Fact]
-        public static void Quit()
-        {
-            GetInstance();
-            _chromeDriver.Close();
-            _chromeDriver.Quit();
-        }
+            string emailToRegister = "tester123@test.com";
+            string courseToRegister = "sap-abap";
 
-        [Fact]
-        private void FillInForm()
-        {
-            GetInstance();
-            _emailText.SendKeys("tester123@gmail.com");
-            var selectedCourse = new SelectElement(_course);
-            selectedCourse.SelectByValue("sap-abap");
-            _submitBtn.Click();
-        }
+            // Act
+            emailText.SendKeys(emailToRegister);
+            var selectedCourse = new SelectElement(course);
+            selectedCourse.SelectByValue(courseToRegister);
+            submitBtn.Click();
 
-        [Fact]
-        private void AssertResult()
-        {
-            GetInstance();
+            // Assert
             IAlert alert = _chromeDriver.SwitchTo().Alert();
 
             string expectedText = "Form Submitted Successfully...";
             string alertText = alert.Text;
 
             alertText.Should().Be(expectedText);
+
+            // Cleaning up
+            _chromeDriver.Close();
+            _chromeDriver.Quit();
         }
     }
 }
